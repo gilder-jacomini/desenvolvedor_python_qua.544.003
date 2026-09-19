59a8aa79e1ebe916553ef30c58869dbaa5f8228a
ALUNO <gilderjacomini@gmail.com>
2026-09-18 21:35:52 -0300

    Aula dia 18/09/2026

--- Parte07-Flask/05_captura_de_texto_ocr/app.py
+++ Parte07-Flask/05_captura_de_texto_ocr/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(index.html)
+    return render_template('index.html')
+
+@app.route('/textoExtraido')
+def extrair_texto():
+    return render_template('extracao.html')
 
 
 if __name__ == '__main__':
